ee31d13edef9ac6dba2cbbf304c07130ca116554
Labmem Xie <141630413+fpstesT00@users.noreply.github.com>
2024-03-18 18:27:39 +0800

    完成轮播图指示点

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-//
+// 引入导航栏
+import customNavbar from './components/customNavbar.vue'
 </script>
 
 <template>
+  <customNavbar />
+  <XtxSwiper />
   <view class="index">index</view>
 </template>
 
