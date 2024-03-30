d50d31008a14dede43d323e12dc38515128dde3d
Labmem Xie <141630413+fpstesT00@users.noreply.github.com>
2024-03-30 17:59:34 +0800

    商品详情页-图片预览模式

---
+++ src/services/goods.ts
@@ -0,0 +1,12 @@
+import { http } from '@/utils/http'
+import type { GoodsResult } from '@/types/goods'
+/**
+ * 商品详情
+ * @param id 商品id
+ */
+export const getGoodsByIdAPI = (id: string) => {
+  return http<GoodsResult>({
+    url: '/goods',
+    data: { id },
+  })
+}
