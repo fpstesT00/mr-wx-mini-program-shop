c75e65f61e4d1c23e6708c7c6685fcca96f5a495
Labmem Xie <141630413+fpstesT00@users.noreply.github.com>
2024-04-01 20:48:34 +0800

    .feat 基本商品详情

---
+++ src/pages/goods/components/PageSkeleton.vue
@@ -0,0 +1,222 @@
+<template name="skeleton">
+  <view class="sk-container">
+    <scroll-view :scroll-y="true" class="viewport viewport" :enable-back-to-top="true">
+      <view class="goods goods">
+        <view class="preview preview">
+          <swiper :circular="true" :current="0" :autoplay="false">
+            <swiper-item
+              style="
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                transform: translate(0%, 0px) translateZ(0px);
+              "
+            >
+              <image mode="aspectFill" class="sk-image"></image>
+            </swiper-item>
+          </swiper>
+          <view class="indicator indicator">
+            <text class="current sk-transparent sk-opacity">1</text>
+            <text class="split sk-transparent sk-opacity">/</text>
+            <text class="total sk-transparent sk-opacity">5</text>
+          </view>
+        </view>
+        <view class="meta meta">
+          <view class="price price">
+            <text class="symbol sk-transparent sk-opacity">¥</text>
+            <text class="number sk-transparent sk-text-14-2857-366 sk-text">4.90</text>
+          </view>
+          <view class="name ellipsis sk-transparent sk-text-14-2857-802 sk-text"
+            >智利老牌名庄，梅洛干红750毫升</view
+          >
+          <view class="desc sk-transparent sk-text-0-0000-336 sk-text"
+            >智利名庄伊拉苏，经典品种甜美柔顺app</view
+          >
+        </view>
+        <view class="action action">
+          <view class="item arrow sk-pseudo sk-pseudo-circle">
+            <text class="label sk-transparent sk-text-14-2857-631 sk-text">选择</text>
+            <text class="text ellipsis sk-transparent sk-text-14-2857-660 sk-text">
+              请选择商品规格
+            </text>
+          </view>
+          <view class="item arrow sk-pseudo sk-pseudo-circle">
+            <text class="label sk-transparent sk-text-14-2857-512 sk-text">送至</text>
+            <text class="text ellipsis sk-transparent sk-text-14-2857-652 sk-text">
+              请选择收获地址
+            </text>
+          </view>
+          <view class="item arrow sk-pseudo sk-pseudo-circle"></view>
+        </view>
+      </view>
+    </scroll-view>
+    <view class="toolbar toolbar" style="padding-bottom: 0px">
+      <view class="icons icons">
+        <button class="icons-button sk-transparent sk-button sk-pseudo sk-pseudo-circle">
+          <text class="icon-heart sk-pseudo sk-pseudo-circle"></text>收藏
+        </button>
+        <button
+          class="icons-button sk-transparent sk-button sk-pseudo sk-pseudo-circle"
+          open-type="contact"
+        >
+          <text class="icon-handset sk-pseudo sk-pseudo-circle"></text>客服
+        </button>
+        <navigator class="icons-button sk-transparent" open-type="switchTab">
+          <text class="icon-cart sk-pseudo sk-pseudo-circle"></text>购物车
+        </navigator>
+      </view>
+      <view class="buttons buttons">
+        <view
+          class="addcart sk-transparent sk-text-32-0513-245 sk-text"
+          style="background-position-x: 50%"
+        >
+          加入购物车
+        </view>
+        <view
+          class="buynow sk-transparent sk-text-32-0513-814 sk-text"
+          style="background-position-x: 50%"
+        >
+          立即购买
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+<style>
+.sk-transparent {
+  color: transparent !important;
+}
+.sk-opacity {
+  opacity: 0 !important;
+}
+.sk-text-14-2857-366 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 76.087rpx;
+  position: relative !important;
+}
+.sk-text {
+  background-origin: content-box !important;
+  background-clip: content-box !important;
+  background-color: transparent !important;
+  color: transparent !important;
+  background-repeat: repeat-y !important;
+}
+.sk-text-14-2857-802 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 43.1159rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-336 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 23.5507rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-631 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 35.5072rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-660 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 35.5072rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-512 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 35.5072rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-652 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 35.5072rpx;
+  position: relative !important;
+}
+.sk-text-32-0513-245 {
+  background-image: linear-gradient(
+    transparent 32.0513%,
+    #eeeeee 0%,
+    #eeeeee 67.9487%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 70.6522rpx;
+  position: relative !important;
+}
+.sk-text-32-0513-814 {
+  background-image: linear-gradient(
+    transparent 32.0513%,
+    #eeeeee 0%,
+    #eeeeee 67.9487%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 70.6522rpx;
+  position: relative !important;
+}
+.sk-button {
+  color: #efefef !important;
+  background: #efefef !important;
+  border: none !important;
+  box-shadow: none !important;
+}
+.sk-image {
+  background: #efefef !important;
+}
+.sk-pseudo::before,
+.sk-pseudo::after {
+  background: #efefef !important;
+  background-image: none !important;
+  color: transparent !important;
+  border-color: transparent !important;
+}
+.sk-pseudo-rect::before,
+.sk-pseudo-rect::after {
+  border-radius: 0 !important;
+}
+.sk-pseudo-circle::before,
+.sk-pseudo-circle::after {
+  border-radius: 50% !important;
+}
+.sk-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: transparent;
+}
+</style>
--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -3,6 +3,10 @@ import { getGoodsByIdAPI } from '@/services/goods'
 import type { GoodsResult } from '@/types/goods'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+import AddressPanel from './components/AddressPanel.vue'
+import ServicePanel from './components/ServicePanel.vue'
+import PageSkeleton from './components/PageSkeleton.vue'
+
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 // 接收页面参数
@@ -34,13 +38,25 @@ const popup = ref<{
   open: (type?: UniHelper.UniPopupType) => void
   close: () => void
 }>()
-onLoad(() => {
-  getGoodsByIdData()
+
+// 弹出层条件渲染
+const popupName = ref<'address' | 'service'>()
+const openPopup = (name: typeof popupName.value) => {
+  // 修改弹出层名称
+  popupName.value = name
+  // 打开弹出层
+  popup.value?.open()
+}
+// 渲染完成标记
+const inFinish = ref(false)
+onLoad(async () => {
+  await getGoodsByIdData()
+  inFinish.value = true
 })
 </script>
 
 <template>
-  <scroll-view scroll-y class="viewport">
+  <scroll-view scroll-y class="viewport" v-if="inFinish">
     <!-- 基本信息 -->
     <view class="goods">
       <!-- 商品主图 -->
@@ -73,11 +89,11 @@ onLoad(() => {
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格 </text>
         </view>
-        <view class="item arrow">
+        <view class="item arrow" @tap="openPopup('address')">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view @tap="popup?.open()" class="item arrow">
+        <view @tap="openPopup('service')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -130,7 +146,8 @@ onLoad(() => {
       </view>
     </view>
   </scroll-view>
-
+  <!-- 骨架屏 -->
+  <PageSkeleton v-else />
   <!-- 用户操作 -->
   <view class="toolbar" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }">
     <view class="icons">
@@ -150,9 +167,8 @@ onLoad(() => {
 
   <!-- uni-弹层 -->
   <uni-popup ref="popup" type="bottom" background-color="#fff">
-    <view>assasa1</view>
-    <view>jsdiaoasd2</view>
-    <button @tap="popup?.close()">关闭</button>
+    <AddressPanel v-if="popupName === 'address'" @close="popup?.close()" />
+    <ServicePanel v-if="popupName === 'service'" @close="popup?.close()" />
   </uni-popup>
 </template>
 
